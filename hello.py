767689947618a0fef352ae185ab05dcdd2bbb832
rachidWassati <rachid.jeffali@wassati.com>
2024-12-18 15:20:28 +0100

    initialisation du projet avec uv

---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from medisecure-api!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_create_patient_folder_usecase.py
@@ -0,0 +1,7 @@
+
+
+
+class TestCreatePatientFolderUseCase:
+
+    def test_should_fail_if_patient_already_exists(self):
+        ...
